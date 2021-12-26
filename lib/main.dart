819ba8a02550f03f01997f7f690e605982b3efd9
Harsh-Sharma-1 <85671456+Harsh-Sharma-1@users.noreply.github.com>
2021-12-26 16:41:47 +0530

    function ,textstyles, routes tutorial.

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 
-import 'home_page.dart';
+import 'pages/home_page.dart';
+import 'pages/login_page.dart';
 
 void main() {
   runApp(MyApp());
@@ -8,11 +9,28 @@ void main() {
 
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
-
   @override
   Widget build(BuildContext context) {
+    functutorial(); //since we have not passed any value so it take default
+    functutorial(price: 20); //since we have passed 20 in it so it will take 20
     return MaterialApp(
-      home: HomePage(),
+      themeMode: ThemeMode.light,
+      theme: ThemeData(
+        primarySwatch: Colors.deepPurple,
+      ),
+      darkTheme: ThemeData(
+        brightness: Brightness.dark,
+      ),
+      initialRoute: "/home",
+      routes: {
+        "/": (context) => LoginPage(),
+        "/home": (context) => HomePage(),
+        "/login": (context) => LoginPage(),
+      },
     );
   }
+
+  functutorial({int price = 50}) {
+    print('$price');
+  } //the price is a optional parameter
 }
